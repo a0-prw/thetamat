065280496ca08561394ef0b6894b2e43cd0986a4
a0-prw <pete_wood@runbox.com>
2013-11-21 10:15:32 +0100

    Added copyright notice and licence GPL V.3 to all files

--- Scripts/reload.lisp
+++ Scripts/reload.lisp
@@ -1,3 +1,22 @@
+;; Copyright 2013 Peter Robert Wood  
+
+;; mail: peter@thetamat.net
+
+;; This file is part of Thetamat.
+
+;; Thetamat is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Thetamat is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with Thetamat.  If not, see <http://www.gnu.org/licenses/>.
+
 (in-package :autotutor)
 
 (load "Scripts/dom-utils.lisp")
